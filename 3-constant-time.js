641d13b7c52a766e34478e630d62683da2544901
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-13 02:53:04 +0700

    Finished item 26. Constant Time

---
+++ 3-constant-time.js
@@ -0,0 +1,11 @@
+function compressFirstBox(boxes) {
+    console.log(boxes[0]);
+}
+
+const boxes = [0, 1, 2, 3, 4, 5];
+
+function logFirstTwoBoxes(boxes) {
+    console.log(boxes[0]); // O(1)
+    console.log(boxes[1]); // O(1)
+}
+logFirstTwoBoxes(boxes); // O(2) -> O(1) -- Constant Time
