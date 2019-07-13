a0fa2a0f823293d0911f0bcd6342741b31564a83
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-13 15:48:28 +0700

    Finished item 42. Exercise: Space Complexity

---
+++ 13-space-complexity.js
@@ -0,0 +1,15 @@
+function booooo(n) {
+    for (let i = 0; i < n.length; i++) {
+        console.log('booooo!');
+    }
+}
+booooo([1, 2, 3, 4, 5]); // O(1)
+
+function arrayOfHiNTime(n) {
+    let hiArray = [];
+    for (let i = 0; i < n; i++) {
+        hiArray[i] = 'hi';
+    }
+    return hiArray;
+}
+console.log(arrayOfHiNTime(6));
