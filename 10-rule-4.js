a91f139e66620087eefbcc3a84095fafccd84e19
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-13 11:30:23 +0700

    Finished item 36. Big O Rule 4

---
+++ 10-rule-4.js
@@ -0,0 +1,20 @@
+const printAllNumbersThenAllPairSums = numbers => {
+
+    console.log('these are the munbers: ');
+    numbers.forEach(number => console.log(number));
+
+    console.log('and these are their sums: ');
+    numbers.forEach(firstNumber => {
+        numbers.forEach(secondNumber => {
+            console.log(firstNumber + secondNumber);
+        });
+    });
+}
+
+printAllNumbersThenAllPairSums([1, 2, 3, 4, 5]);
+// O (n + n^2)
+// O (n^2)
+
+// Ex.
+// O (x^2+3x+100+x/2)
+// O (x^2) -> Drop Non-Dominants term
