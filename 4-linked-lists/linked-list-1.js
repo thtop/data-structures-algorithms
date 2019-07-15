4b7c16dd46b00872165ea5f427ace314f3a65db6
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-16 05:54:19 +0700

    Finished item 92. printList()

--- 4-linked-lists/linked-list-1.js
+++ 4-linked-lists/linked-list-1.js
@@ -1,4 +1,4 @@
-// 1 --> 10 --> 5 --> 16   
+// 1 --> 10 --> 99 --> 5 --> 16   
 
 // let myLinkedList = {
 //     head: {
@@ -42,10 +42,26 @@ class LinkedList {
         this.length++;
         return this;
     }
+
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return array;
+    }
+
+    insert(index, value) {
+        //
+    }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-console.log(myLinkedList);
+console.log(myLinkedList.printList());
+// myLinkedList.insert(2, 99);
+// console.log(myLinkedList);
