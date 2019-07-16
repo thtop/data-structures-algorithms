13b5e8ff1ee00b18da73bcedf2b2b980462915b0
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-16 15:00:08 +0700

    Updated item 109.

--- 5-stacks-queues/main.js
+++ 5-stacks-queues/main.js
@@ -1,1 +1,24 @@
-// Stacks 
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+    peek() {}
+    push(value) {}
+    pop() {}
+    //isEmpty
+}
+
+const myStack = new Stack();
+
+//Discord
+//Udemy
+//google
