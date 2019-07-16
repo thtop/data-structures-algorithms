564d9c20d4b57b07d0b9470e71b7b8695f65c97e
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-16 15:12:02 +0700

    Finished item 110. Solution: Stack Implementation (Linked Lists)

--- 5-stacks-queues/main.js
+++ 5-stacks-queues/main.js
@@ -11,14 +11,46 @@ class Stack {
         this.bottom = null;
         this.length = 0;
     }
-    peek() {}
-    push(value) {}
-    pop() {}
+    peek() {
+        return this.top;
+    }
+    push(value) {
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            const noldingPoiner = this.top;
+            this.top = newNode;
+            this.top.next = noldingPoiner;
+        }
+        this.length++;
+        return this;
+    }
+    pop() {
+        if (!this.top) {
+            return null;
+        }
+        if (this.top === this.bottom) {
+            this.bottom = null;
+        }
+        //const holdingPointer = this.top;
+        this.top = this.top.next;
+        this.length--;
+        return this;
+    }
     //isEmpty
 }
 
 const myStack = new Stack();
-
+//console.log(myStack.peek());
+console.log(myStack.push('google'));
+console.log(myStack.push('udemy'));
+console.log(myStack.push('discors'));
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
 //Discord
 //Udemy
 //google
