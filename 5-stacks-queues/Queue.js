32f06101ebd8777fe4a8b34f37b5b3984757981e
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-16 15:37:15 +0700

    Finished item 114. Solution: Queue Implementation

--- 5-stacks-queues/Queue.js
+++ 5-stacks-queues/Queue.js
@@ -11,15 +11,47 @@ class Queue {
         this.last = null;
         this.length = 0;
     }
-    peek() {}
-    enqueue(value) {}
-    dequeue() {}
+    peek() {
+        return this.first;
+    }
+    enqueue(value) {
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    dequeue() {
+        if (!this.first) {
+            return null;
+        }
+        if (this.first === this.last) {
+            this.last = null;
+        }
+        const holdingPointer = this.first;
+        this.first = this.first.next;
+        this.length--;
+        return this;
+    }
     //isEmpty;
 }
 
 const myQueue = new Queue();
-
-
+//console.log(myQueue.peek());
+console.log(myQueue.enqueue('Joy'));
+console.log(myQueue.enqueue('Matt'));
+console.log(myQueue.enqueue('Pavel'));
+console.log(myQueue.enqueue('Samir'));
+console.log(myQueue.peek());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
 //Joy
 //Matt
 //Pavel
