c015b91976b96ea70aa74537f959467b61fbfd09
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-17 05:41:10 +0700

    Finished item 141. Solution: Graph Implementation

--- 7-graphs/Graph.js
+++ 7-graphs/Graph.js
@@ -3,9 +3,14 @@ class Graph {
         this.numberOfNodes = 0;
         this.adjacentList = {};
     }
-    addVertex(node) {}
+    addVertex(node) {
+        this.adjacentList[node] = [];
+        this.numberOfNodes++;
+    }
     addEdge(node1, node2) {
         //undirected Graph 
+        this.adjacentList[node1].push(node2);
+        this.adjacentList[node2].push(node1);
     }
     showConnections() {
         const allNodes = Object.keys(this.adjacentList);
