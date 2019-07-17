f95bb0ac771d6a70d2141e0ccec82dd26938d195
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-18 05:03:51 +0700

    Finished item 146. Recursion Introduction

---
+++ 8-recursion/inception.js
@@ -0,0 +1,17 @@
+function inception() {
+    inception();
+}
+
+const user = {
+    id: 104,
+    email: 'timmy@gmail.com',
+    presonalInfo: {
+        name: 'timmy',
+        address: {
+            line1: 'westly st',
+            line2: 'washamsher',
+            city: 'wallas',
+            state: 'WY'
+        }
+    }
+}
