f978beb67b65d0547d78741a507200f64e8fb8d8
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-18 05:10:18 +0700

    Finished item 147. Stack Overflow

--- 8-recursion/inception.js
+++ 8-recursion/inception.js
@@ -1,17 +1,18 @@
 function inception() {
+    debugger;
     inception();
 }
 
-const user = {
-    id: 104,
-    email: 'timmy@gmail.com',
-    presonalInfo: {
-        name: 'timmy',
-        address: {
-            line1: 'westly st',
-            line2: 'washamsher',
-            city: 'wallas',
-            state: 'WY'
-        }
-    }
-}
+// const user = {
+//     id: 104,
+//     email: 'timmy@gmail.com',
+//     presonalInfo: {
+//         name: 'timmy',
+//         address: {
+//             line1: 'westly st',
+//             line2: 'washamsher',
+//             city: 'wallas',
+//             state: 'WY'
+//         }
+//     }
+// }
