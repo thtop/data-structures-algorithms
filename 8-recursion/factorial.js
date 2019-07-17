3e4cf2b9388de825424fcfa64edf900d2c089edc
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-18 05:34:12 +0700

    Finished item 150. Solution: Factorial

--- 8-recursion/factorial.js
+++ 8-recursion/factorial.js
@@ -2,11 +2,22 @@
 // One should use recursive, the other should just use a for loop
 
 function findFactorialRecursive(number) {
-    //code here
-    return answer;
+    if (number === 2) {
+        return 2;
+    }
+    return number * findFactorialRecursive(number - 1);
 }
 
 function findFactorialIterative(number) {
-    //code here
+    let answer = 1;
+    if (number === 2) {
+        answer = 2;
+    }
+    for (let i = 2; i <= number; i++) {
+        answer = answer * i
+    }
     return answer;
 }
+
+//console.log(findFactorialIterative(5));
+console.log(findFactorialRecursive(5));
