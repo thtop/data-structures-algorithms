cd884923aa222ea227f54c559354bba1c707cb3a
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-18 20:13:06 +0700

    Finished item 178. Radix Sort + Countin Sort

--- 9-sorting/note.js
+++ 9-sorting/note.js
@@ -1,7 +1,13 @@
-/**
+/** Comparison Sort
  * - Bubble Sort
  * - Insertion Sort
  * - Selecttion Sort
  * - Merge Sort 
  * - Quick Sort
+ */
+
+
+/** Non-Comparison Sort 
+ * - Countion Sort 
+ * - Radix Sort 
  */
