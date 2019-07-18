d98ebe01e2855ea177b9f9aedf27bfb086ae7ec4
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-18 19:34:53 +0700

    Finished item 158. the Issue With sort()

--- 9-sorting/main.js
+++ 9-sorting/main.js
@@ -1,2 +1,26 @@
-const letters = ['a', 'd', 'z', 'e', 'r', 'b'];
-console.log(letters.sort());
+// const letters = ['a', 'd', 'z', 'e', 'r', 'b'];
+// console.log(letters.sort());
+
+const basket = [2, 65, 34, 2, 1, 7, 8];
+console.log(basket.sort());
+
+const sortNumber = basket.sort(function (a, b) {
+    return a - b;
+});
+console.log('sortNumber', sortNumber);
+
+
+
+
+console.log('2'.charCodeAt(0)); // 54
+console.log('65'.charCodeAt(0)); // 54
+console.log('34'.charCodeAt(0)); // 54
+console.log('7'.charCodeAt(0)); // 54
+
+const thai = ['ไก่', 'นก', 'แตงโม', 'มะขาม', 'ขวด', 'กระรอก'];
+console.log('thai: ', thai.sort());
+
+const result = thai.sort(function (a, b) {
+    return a.localeCompare(b);
+});
+console.log('result: ', result)
