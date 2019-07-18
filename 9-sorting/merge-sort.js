e5186b8c54d897b16f2ef57d84d270cdb6c69343
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-18 19:59:30 +0700

    Finished item 172. Solution: Merge Sort

--- 9-sorting/merge-sort.js
+++ 9-sorting/merge-sort.js
@@ -5,6 +5,13 @@ function mergeSort(array) {
         return array
     }
     // Split Array in into right and left
+    const length = array.length;
+    const middle = Math.floor(length / 2)
+    const left = array.slice(0, middle)
+    const right = array.slice(middle)
+    // console.log('left:', left);
+    // console.log('right:', right);
+
 
     return merge(
         mergeSort(left),
@@ -13,7 +20,21 @@ function mergeSort(array) {
 }
 
 function merge(left, right) {
-
+    const result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex < left.length &&
+        rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++
+        }
+    }
+    // console.log(left, right)
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
 
